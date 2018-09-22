3dff635bf0444106ee5fdbfcedf23b0cd2cd5a8a
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-22 18:21:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
 
@@ -9,7 +10,18 @@ class Student < InteractiveRecord
   end
 
   def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
 
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      binding.pry
+      column_names << column['name']
+    end
+
+    column_names.compact
+    
   end
 
   def initialize
