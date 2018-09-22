234cff878649d3a0426cc252145cf5c6cf499531
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-22 18:11:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ require 'interactive_record.rb'
 class Student < InteractiveRecord
 
   def self.table_name
-    self.downcase.pluralized
+    self.to_s.downcase.pluralized
   end
 
   def self.column_names
