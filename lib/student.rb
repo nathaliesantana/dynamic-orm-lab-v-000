000c0e390b58de3dec30ed6ab9d75fff7a9bff36
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-23 11:16:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student < InteractiveRecord
 
     DB[:conn].execute(sql)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 
   end
 
