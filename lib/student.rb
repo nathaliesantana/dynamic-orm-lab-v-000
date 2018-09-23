606bb621f1e9353f9bce7bf70cb993ed97ec073c
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-23 11:22:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student < InteractiveRecord
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{table_name_for_insert} WHERE name = ?"
+    sql = "SELECT * FROM #{table_name} WHERE name = ?"
     DB[:conn].execute(sql, name)
   end
 
