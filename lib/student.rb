1e52500d1aa6ffeeb46dad7b25b53e38d94d2de2
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-23 10:42:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student < InteractiveRecord
 
   def values_for_insert
     values = []
-    self.class.column_names.each do |col_name|
+    self.class.initialize.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
       binding.pry
     end
