3e46c9842c5dc30b3b0bed08b256fa78d276bfbd
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-23 12:35:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
+
 class Student < InteractiveRecord
+self.column_names.each { |attribute| attr_accessor attribute.to_sym }
 
 end
