17261c8debc2cc9bde56a272f043635018a6d245
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-23 10:24:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,6 @@ class Student < InteractiveRecord
 
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join (", ")
-    binding.pry
-
   end
 
   def values_for_insert
